30f2fcc075b57f31e637cf24a5ce8ff3122a792c
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2018-11-07 17:23:08 +0100

    modified default folder

--- src/main/embl/rieslab/mm/uidevint/controller/SystemConstants.java
+++ src/main/embl/rieslab/mm/uidevint/controller/SystemConstants.java
@@ -9,7 +9,7 @@ package main.embl.rieslab.mm.uidevint.controller;
 public class SystemConstants {
 
 	// Write/read
-	public final static String HOME = "ht-SMLM/"; // home of the default configuration file
+	public final static String HOME = "EMU/"; // home of the default configuration file
 	public final static String CONFIG_EXT = "uicfg"; // extension of the configuration file
 	public final static String CONFIG_NAME = HOME+"config."+CONFIG_EXT; // path to the configuration file
 }
