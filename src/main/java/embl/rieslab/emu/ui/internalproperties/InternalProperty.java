2eb23106692383a964bc6039cd103dcc8b263c88
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2019-02-20 18:19:37 +0100

    more javadocs

--- src/main/java/embl/rieslab/emu/ui/internalproperties/InternalProperty.java
+++ src/main/java/embl/rieslab/emu/ui/internalproperties/InternalProperty.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 
 import main.java.embl.rieslab.emu.ui.ConfigurablePanel;
 
+/**
+ * An InternalProperty is aimed at passing information between {@link main.java.embl.rieslab.emu.ui.ConfigurablePanel}s
+ * using an atomic variable of type T. In the ConfigurablePanel, the value can then be retrieved as a wrapper for one of
+ * the corresponding primitive type. An InternalProperty is instantiated with a {@code name}. InternalProperties with the
+ * same name will be shared between the different ConfigurablePanels that created them, as long as they are of the same InternalPropertyType.
+ * <pre>
+ * Upon modification of its value, through a call to {@link #setInternalPropertyValue(Object, ConfigurablePanel)}, the
+ * value of the atomic variable is updated and a call to all other listeners except the source of the call notifies them
+ * of the change.
+ * 
+ * @author Joran Deschamps
+ *
+ * @param <T> Should be an Atomic type to keep the synchronization between threads.
+ * @param <V> Wrapper for a primitive type the Atomic type can be easily converted to.
+ */
 public abstract class InternalProperty<T, V> {
 	private String name_;
 	private T value_;
