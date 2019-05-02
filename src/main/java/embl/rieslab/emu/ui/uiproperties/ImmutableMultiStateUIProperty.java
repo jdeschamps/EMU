417b682e3469258985bbcc4c2235be5ce1038010
Rieslab <7593222+Rieslab@users.noreply.github.com>
2019-05-02 11:06:04 +0200

    typo

--- src/main/java/embl/rieslab/emu/ui/uiproperties/ImmutableMultiStateUIProperty.java
+++ src/main/java/embl/rieslab/emu/ui/uiproperties/ImmutableMultiStateUIProperty.java
@@ -136,7 +136,7 @@ public class ImmutableMultiStateUIProperty extends UIProperty {
 	 * 
 	 * @return State names
 	 */
-	public String[] getStatesNam(){
+	public String[] getStatesName(){
 		return states_.keySet().toArray(new String[states_.size()]);
 	}	
 	
