5b2059eb5bec84a43d318215adcaa9932c369555
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2019-08-12 14:38:05 +0200

    updated pom

--- deployMM2toMvn.sh
+++ deployMM2toMvn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Simple bash script to add the main Micro-manager jars to Maven.
-MM2_PLUGINS_HOME="D:\Micromanager\Micro-Manager-2.0gamma\plugins\Micro-Manager"
+# Set your Micro-manager path here:
+MM2_PLUGINS_HOME="D:\Micromanager\Micro-Manager-2.0gammaNB190725\plugins\Micro-Manager"
 
 mvn install:install-file -Dfile="$MM2_PLUGINS_HOME\MMJ_.jar" -DgroupId=org.micromanager  -DartifactId=MMJ_ -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile="$MM2_PLUGINS_HOME\MMAcqEngine.jar" -DgroupId=org.micromanager  -DartifactId=MMAcqEngine -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar
